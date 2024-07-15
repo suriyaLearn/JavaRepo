09744e89160a77b22b3e9ae22784f2ba644b8ad4
Suriya Kumar <suriya4prof@gmail.com>
2024-07-15 09:54:21 +0530

    initial commit

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class InputOutput {
+import java.util.Scanner;
+
+public class InputOutput {
+    Scanner input = new Scanner(System.in);
+
+    public static void main (String[] args){
+        InputOutput ob = new InputOutput();
+//        ob.getUserInputString();
+        ob.getUserInputDecimal();
+    }
+
+    public void  getUserInputString(){
+        System.out.println("Enter your User Input as String");
+        String userInput = input.nextLine();
+        System.out.println("User has inputed the Text :"+userInput);
+    }
+
+    public void  getUserInputDecimal(){
+        System.out.println("Enter your User Input as Decimal");
+        float userInput = input.nextFloat();
+        System.out.println("User has inputed the Decimal :"+userInput);
+    }
+
 }
--- src/Loops.java
+++ src/Loops.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Loops {
+public class Loops {
+
+    public static void main(String[] args) {
+        Loops obj = new Loops();
+//        obj.forLoopDemo();
+        obj.ifElseLoop();
+    }
+
+    public void forLoopDemo() {
+        int random = 10;
+
+        for (int i = 0; i < random; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public void ifElseLoop(){
+        String name = "sur";
+        if(name.equals("smur")){
+            System.out.println("Found");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
+
 }
